d6764f11e0ea65b9e0e3c32d14c788825914b3c0
Rhythm Varshney <rhythm-design>
2021-08-26 00:04:54 +0530

    redux store configured

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
+import { addTodo, deleteTodo, removeTodo } from "../actions/index";
 import "./Todo.css";
 
 function Todo(){
+
+    const [inputData,setInputData]=useState("");
+    const dispatch=useDispatch();
+    
     return(
         <div className="main-div" >
         <div className="child-div">
@@ -11,8 +17,11 @@ function Todo(){
         </figure>
 
         <div className="addItems">
-            <input type="text" placeholder="Add Items" />
-            <i className="fa fa-plus add-btn"></i>
+            <input type="text" placeholder="Add Items" 
+            value={inputData}
+            onChange={(event)=>setInputData(event.target.value)}
+             />
+            <i className="fa fa-plus add-btn" onClick={()=>dispatch(addTodo(inputData))}></i>
         </div>
 
         </div>
